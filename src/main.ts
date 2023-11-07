f54e5ee3afd126f6aef4d47f354f2fa8735df5e6
MahmoudRadwan404 <mahmoudrdwan32@gmail.com>
2023-11-07 20:06:46 +0200

    readme.md

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import "./routes"
 import {connection} from"./database/connection"
-import {loadEnv} from '@mongez/dotenv'
-loadEnv()
-
+//import {loadEnv} from '@mongez/dotenv'
+//loadEnv()
+import dotenv from 'dotenv'
+dotenv.config()
 
 const key=process.env.testData
 console.log(key)
