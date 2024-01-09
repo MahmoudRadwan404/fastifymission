7650b6b8e47e081ce9aa6d261ab3231afa7cd675
MahmoudRadwan404 <mahmoudrdwan32@gmail.com>
2024-01-09 13:44:27 +0200

    commit

--- src/controllers/users/get-all-users.ts
+++ src/controllers/users/get-all-users.ts
@@ -3,7 +3,7 @@ import { collection } from "../../database/connection";
 
 const getAllUsers = async (request: FastifyRequest, reply: FastifyReply) => {
   const users = collection("users");
-  const data: any = await users.find({}).toArray();
-  reply.status(200).send(data);
+  const usersData: any = await users.find({}).toArray();
+  reply.status(200).send(usersData);
 };
 export default getAllUsers;
